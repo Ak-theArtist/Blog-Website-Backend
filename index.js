cba688d5952f0c5fad32fee2ec69103d84814aee
Akash Kumar <kumarakash91384@gmail.com>
2024-09-20 09:26:14 +0530

    commited mongo atlas server

--- index.js
+++ index.js
@@ -26,12 +26,7 @@ app.use(express.static('public'))
 
 
 // mongoose.connect('mongodb://127.0.0.1:27017/blog')
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    tls: true,
-    tlsAllowInvalidCertificates: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected successfully"))
 .catch(err => console.error("MongoDB connection error:", err));
 
