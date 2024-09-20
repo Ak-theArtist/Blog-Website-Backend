b412be3ee9fd65e41125bbde1a7ae4075a9da712
Akash Kumar <kumarakash91384@gmail.com>
2024-09-20 12:15:16 +0530

    final

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const storage = multer.diskStorage({
         cb(null, 'Public/Images');
     },
     filename: (req, file, cb) => {
-        cb(null, 'file.fieldname' + "_" + Date.now() + path.extname(file.originalname));
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
     }
 });
 
